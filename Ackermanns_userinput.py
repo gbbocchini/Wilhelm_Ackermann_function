4eec75d440353f1f4b5a3cf119e8a88d9e1a28a1
Gabriel Bocchini <gabrielbocchini@gmail.com>
2018-12-13 10:12:38 -0200

    loading added

--- Ackermanns_userinput.py
+++ Ackermanns_userinput.py
@@ -1,10 +1,11 @@
 import time
 import sys
+from progressbar import ProgressBar, Percentage, Bar
 
 sys.setrecursionlimit(4096*10)
 
 
-print("\nWelcome to the Ackmermann function! History: https://en.wikipedia.org/wiki/Ackermann_function \n\nAdvice: use only positive integers, most systems have stack protection...but you never know (if you use floats or negatives, boy, you are in for hell)!\n\nIf no result is shown, it´s because your system is protecting the memory stack, if your computer crashes or takes a looooong time, it´s because your stack is unprotected and it is trying to figure out the mess. It can eventualy crash also, lol\n\nThe more RAM you have, more results you will see!\n\n")
+print("\nWelcome to the Ackmermann function! History: https://en.wikipedia.org/wiki/Ackermann_function \n\nAdvice: use only positive integers, most systems have stack protection...but you never know (if you use floats or negatives, boy, you are in for hell)!\n\nIf no result is shown, it´s because your system is protecting the memory stack, if your computer crashes or takes a looooong time, it´s because your stack is unprotected and it is trying to figure out the mess. It can eventualy crash also, lol\n\nThe more RAM you have, more results \n\n")
 
 m = int(input("Choose a positive integer: "))
 n = int(input("OK, another one now: "))
@@ -18,11 +19,12 @@ def ack(m,n):
     else:
         return ack(m-1, ack(m,n-1))
 
-animation = "."
-for i in range(5):
-    time.sleep(1)
-    sys.stdout.write("\r" + animation[i % len(animation)])
-    sys.stdout.flush()
-    print("Calculating")
+progress = ProgressBar(widgets=[Percentage(), Bar()], maxval = 500000).start()
+progvar = 0
 
-print("Ackermanns for {} and {} is: {}".format(m, n, ack(m,n)))
+for i in range(500000):
+    calc = ack(m,n)
+    progress.update(progvar + 1)
+    progvar += 1
+
+print("\nAckermanns for {} and {} is: {}".format(m, n, calc))
